db0fa8a148a4015b5a4b92f6180e730b317e39f7
xby1210825 <45911655+xby1210825@users.noreply.github.com>
2018-12-16 19:19:06 +0800

    Add files via upload

---
+++ springboot-demo/src/main/java/cn/itcast/demo/conntroller/HelloController.java
@@ -0,0 +1,19 @@
+package cn.itcast.demo.conntroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String sayHello(){
+
+        return "hello";
+    }
+
+
+}
